b7d9949447715d6c976bf46c9c809561e46f75e1
David Coallier <dev@cloudsplit.com>
2010-03-01 18:07:03 +0000

    - Added the initial CouchDB-Lucene searches by title and content

--- models/SphereView.php
+++ models/SphereView.php
@@ -39,7 +39,43 @@ class SphereView extends \lithium\data\Model {
 					}'
 				)
 			)
-		)
+		),
+        /**
+         * For the search we are going to do a very simple
+         * saerch to start with a very simple search that
+         * will return all values from within the title and
+         * from the content of a post, for a searched term. 
+         */
+        'search' => array(
+            'id'       => '_design/search',
+            'fulltext' => array(
+                // Search by the post title
+                'by_title' => array(
+                    'index' => 'function(doc) {
+                        var ret = new Document();
+                        if (doc.type && doc.type == "post" && doc.title) {
+                            ret.add(doc.title);
+                        }
+
+                        return ret;
+                    }'
+                ),
+
+                // Search by the post content
+                'by_content' => array(
+                    'index' => 'function(doc) {
+                        var ret = new Document();
+                        if (doc.type && doc.type == "post" && doc.type) {
+                            ret.add(doc.content);
+                        }
+
+                        return ret;
+                    }'
+                ),
+
+
+            ),
+        ),
 	);
 
 	public static function create($data = 'latest') {
