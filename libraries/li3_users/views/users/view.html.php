34ff0a9518e3a5109045c1f056bf9dfc0cd9f53a
Jon Adams <pointlessjon@gmail.com>
2011-09-14 07:27:05 -0700

    more copyright updates

--- libraries/li3_users/views/users/view.html.php
+++ libraries/li3_users/views/users/view.html.php
@@ -1,2 +1,11 @@
+<?php
+/**
+ * Lithium Sphere: communized sphere of influence
+ *
+ * @copyright     Copyright 2011, Union of RAD (http://union-of-rad.org)
+ * @license       http://www.opensource.org/licenses/MIT The MIT License
+ */
+?>
+
 <h1><?php echo $user->_id;?></h1>
 <?php echo $user->email;?>
--- views/users/login.html.php
+++ views/users/login.html.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Lithium Sphere: communized sphere of influence
+ *
+ * @copyright     Copyright 2011, Union of RAD (http://union-of-rad.org)
+ * @license       http://www.opensource.org/licenses/MIT The MIT License
+ */
+
 if (!empty($user)) {
 	echo "<h1>" . $this->User->greeting($user) . "</h1>";
 	if (!empty($return)) {
